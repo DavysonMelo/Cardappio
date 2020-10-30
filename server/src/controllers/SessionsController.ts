876e333d7b009c4d65a948657724fcc157739cfe
danielbpc2 <danielbpc2@hotmail.com>
2020-10-30 08:33:50 -0300

    Basic controller structure for session and user

---
+++ server/src/controllers/SessionsController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+
+class SessionController {
+  public async login(request: Request, response: Response): Promise<Response> {
+    try {
+      const { username, password } = request.body;
+      //authentication
+
+      // returns user.
+      return response.json({});
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
+  }
+
+  public async logout(request: Request, response: Response): Promise<Response> {
+    return response.json();
+  }
+}
+
+export default SessionController;
---
+++ server/src/controllers/UserController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+
+class UserController {
+  public async show(request: Request, response: Response): Promise<Response> {
+    return response.json();
+  }
+
+  public async create(request: Request, response: Response): Promise<Response> {
+    return response.json();
+  }
+
+  public async index(request: Request, response: Response): Promise<Response> {
+    return response.json();
+  }
+
+  public async delete(request: Request, response: Response): Promise<Response> {
+    return response.json();
+  }
+}
+
+export default UserController;
