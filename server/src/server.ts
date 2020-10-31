b3ff223b1cd13afa302081d84492bb8382fe99f3
DavysonMelo <mdavyson@gmail.com>
2020-10-31 15:36:55 -0300

    Fix: DB connection string fixed

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,7 +6,7 @@ import express from 'express';
 const app = express();
 
 const db = mongoose.connect(
-  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cardappio.vbk5u.mongodb.net/@cardappio?retryWrites=true&w=majority`,
+  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cardappio.vbk5u.mongodb.net/cardappio?retryWrites=true&w=majority`,
   { useNewUrlParser: true }
 );
 
