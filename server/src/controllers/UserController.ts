b2a035e3fa8bc2bcf8408067f798a7c32060f7cd
DavysonMelo <mdavyson@gmail.com>
2020-11-05 14:41:27 -0300

    Feat: User Controller with its CRUD finished

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -1,16 +1,56 @@
 import { Request, Response } from 'express';
+import User from '../models/User';
+import hashPass from '../controllers/hashPass';
 
 class UserController {
-  public async show(request: Request, response: Response): Promise<Response> {
-    return response.json();
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { userName, password, role } = request.body;
+
+    let user = await User.findOne({ userName });
+
+    try {
+      const hashedPassword = await hashPass(password);
+      if (user) {
+        response.status(400).json({ error: 'User already exists!' });
+      } else {
+        user = await User.create({
+          userName,
+          password: hashedPassword,
+          role,
+        });
+      }
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
+    user.password = undefined;
+    return response.status(200).json(user);
   }
 
   public async index(request: Request, response: Response): Promise<Response> {
-    return response.json();
+    let users;
+    try {
+      users = await User.find();
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
+
+    return response.status(200).json(users);
   }
 
   public async delete(request: Request, response: Response): Promise<Response> {
-    return response.json();
+    const { id } = request.params;
+
+    try {
+      const user = User.findById(id);
+      if (!user) {
+        return response.status(400).json({ error: 'User not found!' });
+      } else {
+        await User.findByIdAndDelete(id);
+      }
+      return response.status(200).json({ message: 'User deleted!' });
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
