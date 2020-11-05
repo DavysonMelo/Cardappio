627924523a6b61e5fbf904bc8a9c0905f8163138
DavysonMelo <mdavyson@gmail.com>
2020-11-05 14:48:21 -0300

    Feat: Session controller with login finished

--- server/src/controllers/SessionsController.ts
+++ server/src/controllers/SessionsController.ts
@@ -1,19 +1,29 @@
 import { Request, Response } from 'express';
+import User, { IuserDoc } from '../models/User';
+import bcrypt from 'bcrypt';
 
 class SessionController {
   public async login(request: Request, response: Response): Promise<Response> {
+    let user: IuserDoc | null;
+
     try {
-      const { username, password } = request.body;
-      //authentication
-      // returns user.
-      return response.json({});
+      const { userName, password } = request.body;
+
+      user = await User.findOne({ userName }).select('+password');
+
+      if (!user) {
+        return response.status(400).json('User not found!');
+      }
+
+      if (!(await bcrypt.compare(password, user.password as string))) {
+        return response.status(400).json({ error: 'Invalid password!' });
+      }
+
+      user.password = '';
+      return response.json(user);
     } catch (error) {
       return response.status(400).json({ error: error.message });
     }
-  }
-
-  public async logout(request: Request, response: Response): Promise<Response> {
-    return response.json();
   }
 }
 
