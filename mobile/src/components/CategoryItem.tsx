d3df5408b7e8510322cf1525b24cb3ff55b82eec
NetoJL <zeneto2604@gmail.com>
2020-11-10 15:24:37 -0300

    CategoryItem component finished

---
+++ mobile/src/components/CategoryItem.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { TouchableOpacity, RectButton } from 'react-native-gesture-handler';
+
+import styles from '../styles/categoryItem';
+
+interface CategoryTitle {
+  name: string;
+}
+
+export default function BoxCategory({ name }: CategoryTitle) {
+  return (
+    <RectButton style={styles.container}>
+      <Text>{name}</Text>
+    </RectButton>
+  );
+}
---
+++ mobile/src/styles/categoryItem.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    width: 123,
+    height: 34,
+    margin: 10,
+    marginLeft: 3,
+    borderRadius: 10,
+    backgroundColor: '#F5F4CD',
+    color: '#474747',
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    elevation: 5,
+  },
+});
+
+export default styles;
