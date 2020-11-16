fbff185e7590840f4dee45edbd63041d5858f45f
DavysonMelo <mdavyson@gmail.com>
2020-11-16 14:24:05 -0300

    Fix: Drag function finished

--- web/src/components/OrderCard.tsx
+++ web/src/components/OrderCard.tsx
@@ -1,59 +1,64 @@
-import React from 'react';
-import { useDrag, useDragLayer } from 'react-dnd';
+import React, { useContext } from 'react';
+import { useDrag } from 'react-dnd';
+import ITEM_TYPE from '../types/item';
 
 import orderCropImage from '../assets/images/orderCrop.png';
-import {
-  CardContainer,
-  OrderIdTable,
-  CardContent,
-  ImageCrop,
-  Observations,
-  OrderInfoContainer,
-  OrderRow,
-} from '../styles/ts/orderCard';
+import '../styles/components/orderCard.css';
+import Order from '../types/order';
+import BoardContext from './boardContext';
 
 export interface OrderCardProps {
   className: string;
   isDragging?: boolean;
+  item: Order;
+  index: number;
+  listIndex: number;
 }
 
 const OrderCard: React.FC<OrderCardProps> = (props) => {
-  const [{ isDragging }, dragRef] = useDrag({
-    item: { type: 'CARD' },
+  const { setCardIdx, setDraggedListIdx } = useContext(BoardContext);
+  const index = props.index;
+  const listIndex = props.listIndex;
+  const [{ isDragging }, drag] = useDrag({
+    item: { type: ITEM_TYPE, index, listIndex },
     collect: (monitor) => ({
       isDragging: monitor.isDragging(),
     }),
+    begin: () => {
+      setCardIdx(index);
+      setDraggedListIdx(listIndex);
+    },
   });
 
   return (
-    <CardContainer
+    <div
       id="card-container"
+      ref={drag}
+      style={
+        isDragging
+          ? { opacity: 0, cursor: 'grabbing' }
+          : { opacity: 1, cursor: 'grab' }
+      }
       className={props.className}
-      ref={dragRef}
-      isDragging={isDragging}
     >
-      <CardContent>
-        <OrderIdTable>
-          <span>Pedido #1</span>
-          <span>Mesa: 1</span>
-        </OrderIdTable>
+      <div id="card-content">
+        <div id="order-id-table">
+          <span>Pedido #{props.item.number}</span>
+          <span>Mesa: {props.item.tableNumber}</span>
+        </div>
 
-        <OrderInfoContainer>
-          <OrderRow>
+        <div id="order-info-container">
+          <div id="order-row">
             <h5>Hamburguer</h5>
-            <Observations>(Sem maionese, sem ketchup)</Observations>
-          </OrderRow>
-          <OrderRow>
-            <h5>Guaraná (500ml)</h5>
-            <Observations>(Com Gelo)</Observations>
-          </OrderRow>
-        </OrderInfoContainer>
-      </CardContent>
+            <span className="observations">({props.item.observations})</span>
+          </div>
+        </div>
+      </div>
 
-      <ImageCrop>
+      <div id="image-crop">
         <img src={orderCropImage} alt="Card crop" />
-      </ImageCrop>
-    </CardContainer>
+      </div>
+    </div>
   );
 };
 
