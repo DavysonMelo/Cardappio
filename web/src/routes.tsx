a5168f40c4da86d37d68d87ca8f50e599edf64aa
DavysonMelo <mdavyson@gmail.com>
2020-11-16 14:14:57 -0300

    Feat: Kanban board context added to routes file

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { ModalVisibilityProvider } from './components/modalContext';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
 
 import Admin from './pages/Admin';
 import Login from './pages/Login';
 import Kitchen from './pages/Kitchen';
+import { BoardProvider } from './components/boardContext';
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Login} />
-        <DndProvider backend={HTML5Backend}>
+        <BoardProvider>
           <Route path="/kitchen" component={Kitchen} />
-        </DndProvider>
-        <ModalVisibilityProvider>
-          <Route path="/admin" component={Admin} />
-        </ModalVisibilityProvider>
+          <ModalVisibilityProvider>
+            <Route path="/admin" component={Admin} />
+          </ModalVisibilityProvider>
+        </BoardProvider>
       </Switch>
     </BrowserRouter>
   );
