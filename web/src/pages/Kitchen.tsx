90002cd27e04d0ee6fbce90607a3c1b35cbf4cfa
DavysonMelo <mdavyson@gmail.com>
2020-11-26 10:30:18 -0300

    Screens Admin, Kitchen, Login finished

--- web/src/pages/Kitchen.tsx
+++ web/src/pages/Kitchen.tsx
@@ -1,5 +1,6 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import socketio from 'socket.io-client';
 import { Icon } from 'ts-react-feather-icons';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
@@ -12,38 +13,74 @@ import logoKitchen from '../assets/logoKitchen.svg';
 
 import '../styles/pages/kitchen.css';
 import orderList from '../types/orderList';
-
-let data: orderList;
-
-data = {
-  orders: [
-    {
-      id: '12312312',
-      number: 1,
-      dishID: '1',
-      tableNumber: 1,
-      observations: 'bla bla bla',
-      status: 'recebido',
-    },
-    {
-      id: '12312313',
-      number: 2,
-      dishID: '1',
-      tableNumber: 2,
-      observations: 'oi oi oi ',
-      status: 'recebido',
-    },
-  ],
-};
+import api from '../services/api';
+import UserInterface from '../types/user';
+import Order from '../types/order';
 
 function Kitchen() {
   const history = useHistory();
   const { lists, loadList } = useContext(BoardContext);
+  const [orders, setOrders] = useState<Order[]>([] as Order[]);
+  const [ordersPrep, setOrdersPrep] = useState<Order[]>([] as Order[]);
+  const [ordersDone, setOrdersDone] = useState<Order[]>([] as Order[]);
+  const [user, setUser] = useState<UserInterface>({} as UserInterface);
+
+  async function getOrders() {
+    const response = await api.get('orders', {
+      headers: { status: 'recebido' },
+    });
+    const resp = await api.get('orders', {
+      headers: { status: 'preparando' },
+    });
+    const res = await api.get('orders', {
+      headers: { status: 'pronto' },
+    });
+    let list: Order[];
+    let listPrep: Order[];
+    let listDone: Order[];
+    list = response.data;
+    listPrep = resp.data;
+    listDone = res.data;
+    if (list.length !== 0) {
+      list.map((item) => {
+        setOrders((old) => [...old, item]);
+      });
+    }
+    if (listPrep.length !== 0) {
+      listPrep.map((item) => {
+        setOrdersPrep((old) => [...old, item]);
+      });
+    }
+    if (listDone.length !== 0) {
+      listDone.map((item) => {
+        setOrdersDone((old) => [...old, item]);
+      });
+    }
+  }
+
+  function getUser() {
+    const res = localStorage.getItem('user');
+    if (res) {
+      const parsedUser = JSON.parse(res);
+      setUser(parsedUser);
+    }
+  }
 
   useEffect(() => {
-    const receivedList = data;
-    const preparingList = { orders: [] };
-    const doneList = { orders: [] };
+    getUser();
+    getOrders();
+    const socket = socketio('http://localhost:3333', {
+      transports: ['websocket'],
+    });
+    socket.on('order', async (order: Order) => {
+      setOrders((old) => [...old, order]);
+    });
+  }, []);
+
+  useEffect(() => {
+    const receivedList = { orders: orders };
+    const preparingList = { orders: ordersPrep };
+    const doneList = { orders: ordersDone };
     function setData() {
       loadList([
         receivedList as orderList,
@@ -52,7 +89,7 @@ function Kitchen() {
       ]);
     }
     setData();
-  }, []);
+  }, [orders, ordersPrep, ordersDone]);
 
   function swalPopUp() {
     swal({
@@ -67,60 +104,67 @@ function Kitchen() {
       },
     }).then((willConfirm) => {
       if (willConfirm) {
+        localStorage.clear();
         history.push('/');
       }
     });
   }
 
   return (
-    <DndProvider backend={HTML5Backend}>
-      <div id="kitchen-container">
-        <header>
-          <div>
-            <img src={logoKitchen} alt="Logo cardappio" />
-          </div>
-          <div>
-            <button onClick={swalPopUp}>
-              <Icon name="log-out" size={30} color="#FFF" />
-            </button>
-          </div>
-        </header>
+    <>
+      {user && user.role === 'kitchen' ? (
+        <DndProvider backend={HTML5Backend}>
+          <div id="kitchen-container">
+            <header>
+              <div>
+                <img src={logoKitchen} alt="Logo cardappio" />
+              </div>
+              <div>
+                <button onClick={swalPopUp}>
+                  <Icon name="log-out" size={30} color="#FFF" />
+                </button>
+              </div>
+            </header>
 
-        <div id="status-title-container">
-          <div
-            className="status-title noTextSelect"
-            style={{ backgroundColor: '#CE2424' }}
-          >
-            <span>Recebido</span>
+            <div id="status-title-container">
+              <div
+                className="status-title noTextSelect"
+                style={{ backgroundColor: '#CE2424' }}
+              >
+                <span>Recebido</span>
+              </div>
+              <div
+                className="status-title noTextSelect"
+                style={{ backgroundColor: '#FDCF2E' }}
+              >
+                <span>Preparando</span>
+              </div>
+              <div
+                className="status-title noTextSelect"
+                style={{ backgroundColor: '#36AC42' }}
+              >
+                <span>Pronto</span>
+              </div>
+            </div>
+            <div id="order-status-container">
+              {lists !== undefined && lists.length > 0 ? (
+                lists.map((item, index) => (
+                  <ListContainer
+                    key={index}
+                    index={index}
+                    orderList={item.orders}
+                  />
+                ))
+              ) : (
+                <h1>Nenhum pedido</h1>
+              )}
+            </div>
           </div>
-          <div
-            className="status-title noTextSelect"
-            style={{ backgroundColor: '#FDCF2E' }}
-          >
-            <span>Preparando</span>
-          </div>
-          <div
-            className="status-title noTextSelect"
-            style={{ backgroundColor: '#36AC42' }}
-          >
-            <span>Pronto</span>
-          </div>
-        </div>
-        <div id="order-status-container">
-          {lists !== undefined && lists.length > 0 ? (
-            lists.map((item, index) => (
-              <ListContainer
-                key={index}
-                index={index}
-                orderList={item.orders}
-              />
-            ))
-          ) : (
-            <h1>Nenhum pedido</h1>
-          )}
-        </div>
-      </div>
-    </DndProvider>
+        </DndProvider>
+      ) : (
+        <>{() => history.push('/')}</>
+      )}
+    </>
   );
 }
 
--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import api from '../services/api';
+import UserInterface from '../types/user';
 
 import '../styles/global.css';
 import '../styles/pages/login.css';
 
 import logo from '../assets/logoLogin.svg';
+import swal from 'sweetalert';
 
 function Login() {
+  const [userName, setUserName] = useState<String>('');
+  const [password, setPassword] = useState<String>('');
+  const history = useHistory();
+
+  function swalPopUp() {
+    swal({
+      title: 'Usuário não encontrado',
+      text: 'Nome de usuário ou a senha estão incorretos',
+      icon: 'error',
+      buttons: {
+        confirm: { visible: true, text: 'Ok' },
+      },
+    });
+  }
+
+  async function login(e: FormEvent) {
+    e.preventDefault();
+    try {
+      const response = await api.post('sessions', {
+        userName,
+        password,
+      });
+      localStorage.setItem('user', JSON.stringify(response.data));
+      navigateTo(response.data);
+    } catch (error) {
+      swalPopUp();
+      setUserName('');
+      setPassword('');
+    }
+  }
+
+  function navigateTo(user: UserInterface) {
+    console.log(user.role);
+    if (user.role === 'administrator') {
+      history.push('/admin', user);
+    } else if (user.role === 'kitchen') {
+      history.push('/kitchen', user);
+    }
+  }
+
   return (
     <div id="container">
       <div id="left-sidebar">
@@ -15,14 +59,32 @@ function Login() {
       <div id="login-container">
         <label id="login-label">LOGIN</label>
         <div id="login-box">
-          <form action="">
+          <form>
             <label className="label">Usuário</label>
-            <input className="input" type="text" name="user" />
+            <input
+              className="input"
+              type="text"
+              name="user"
+              value={userName as string}
+              onChange={(e) => {
+                setUserName(e.target.value);
+              }}
+            />
 
             <label className="label">Senha</label>
-            <input className="input" type="password" name="password" />
+            <input
+              className="input"
+              type="password"
+              name="password"
+              value={password as string}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
 
-            <button type="submit">Entrar</button>
+            <button type="submit" onClick={login}>
+              Entrar
+            </button>
           </form>
         </div>
       </div>
