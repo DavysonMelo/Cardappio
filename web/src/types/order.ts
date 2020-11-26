a4b3190b247adcd1b9f410e73d1cdaac15bae198
Daniel Carvalho <danielbpc2@hotmail.com>
2020-11-26 11:12:14 -0300

    fix conflicts?

--- web/src/types/order.ts
+++ web/src/types/order.ts
@@ -1,12 +1,7 @@
 export default interface Order {
   _id: string;
   number: number;
-<<<<<<< HEAD
-  dishID: string;
-  additional: string[];
-=======
   dishName: string[];
->>>>>>> 90002cd27e04d0ee6fbce90607a3c1b35cbf4cfa
   tableNumber: number;
   observations: string[];
   additional?: string[];
